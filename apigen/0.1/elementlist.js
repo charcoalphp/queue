4a5ea24841a3aa818fc892b8036e69a553c6169e
travis-ci <travis@travis-ci.org>
2016-05-19 12:24:28 +0000

    ApiGen (Travis Build : 17  - Branch : 0.1)

---
+++ apigen/0.1/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Charcoal\\Queue\\AbstractQueueManager"],["c","Charcoal\\Queue\\QueueableInterface"],["c","Charcoal\\Queue\\QueueableTrait"],["c","Charcoal\\Queue\\QueueItemInterface"],["c","Charcoal\\Queue\\QueueItemTrait"],["c","Charcoal\\Queue\\QueueManagerInterface"]];
